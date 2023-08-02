fb8ec0ba6ca739236416bedcc380287204d38a14
mintmin <mintmin0320@gmail.com>
2023-08-02 23:33:39 +0900

    feat: 피드, 좌측 사이드메뉴 UI

---
+++ frontend/src/components/home/StoryList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function StoryList() {
+
+  return (
+    <Container>
+      스토리
+    </Container>
+  );
+};
+
+export default StoryList;
+
+const Container = styled.header`
+  width: 100%;
+  height: 130px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
--- frontend/src/pages/home/HomePage.js
+++ frontend/src/pages/home/HomePage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { styled } from 'styled-components';
+
+import Home from '../../components/home/Home';
+import SideBar from '../../components/layout/SideBar';
+import StoryList from '../../components/home/StoryList';
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <Container>
+      <SideBar />
+      <HomeContent>
+        <StoryList />
+        <Home />
+      </HomeContent>
+    </Container>
   )
 }
 
 export default HomePage;
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+const HomeContent = styled.div`
+  width: 53.5%;
+  padding-left: 17.7%;
+  
+`;
