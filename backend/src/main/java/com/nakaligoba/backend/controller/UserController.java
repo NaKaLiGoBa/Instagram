3c31f0946c971685c4096c219e5ba45fb9f78b0f
Daeeun Ko <7273322@naver.com>
2023-08-03 14:23:00 +0900

    feat: 회원가입 API 및 입력 검증

---
+++ backend/src/main/java/com/nakaligoba/backend/controller/UserController.java
@@ -0,0 +1,70 @@
+package com.nakaligoba.backend.controller;
+
+import com.nakaligoba.backend.domain.User;
+import com.nakaligoba.backend.service.UserService;
+import com.nakaligoba.backend.service.UserService.UserDto;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@RequestMapping("/api/v1/users")
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@Valid @RequestBody SignupRequest request) {
+        UserDto userDto = UserDto.builder()
+                .email(request.email)
+                .password(request.password)
+                .fullname(request.fullname)
+                .username(request.username)
+                .build();
+
+        User user = userService.create(userDto);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResult> handleBadRequest(IllegalArgumentException ex) {
+        ErrorResult errorResult = new ErrorResult(ex.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResult);
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    static class SignupRequest {
+
+        @Email
+        private String email;
+
+        @NotBlank
+        private String password;
+
+        @NotBlank
+        private String username;
+
+        @NotBlank
+        private String fullname;
+    }
+
+    private static class ErrorResult {
+        private String message;
+
+        public ErrorResult(String message) {
+            this.message = message;
+        }
+    }
+}
