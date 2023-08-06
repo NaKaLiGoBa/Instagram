db822f0cd0140dc1a5eb3013b014ae6c576c4fe1
Daeeun Ko <7273322@naver.com>
2023-08-07 01:18:39 +0900

    feat: 인증 API 구현

---
+++ backend/src/main/java/com/nakaligoba/backend/controller/AuthController.java
@@ -0,0 +1,53 @@
+package com.nakaligoba.backend.controller;
+
+import com.nakaligoba.backend.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
+@RequestMapping("/api/v1/auth")
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<Void> signin(
+            @Valid @RequestBody SigninRequest request,
+            HttpSession session,
+            HttpServletResponse response
+    ) {
+        if (authService.authenticate(request.id, request.password)) {
+            UUID uuid = UUID.randomUUID();
+
+            session.setAttribute("session_id", uuid);
+
+            Cookie cookie = new Cookie("session_id", uuid.toString());
+            cookie.setMaxAge(3600);
+            response.addCookie(cookie);
+            return ResponseEntity.ok().build();
+        }
+
+        throw new IllegalArgumentException("인증 실패");
+    }
+
+    static class SigninRequest {
+
+        @NotBlank
+        private String id;
+
+        @NotBlank
+        private String password;
+    }
+}
