7bbe07be9b76b21272746680fecb15e93da77e45
Gukin Han <gukin.dev@gmail.com>
2023-08-07 15:29:10 +0900

    feat: 커스텀 쿼리 작성 - postId로 모두 삭제

--- backend/src/main/java/com/nakaligoba/backend/repository/PhotoRepository.java
+++ backend/src/main/java/com/nakaligoba/backend/repository/PhotoRepository.java
@@ -2,6 +2,8 @@ package com.nakaligoba.backend.repository;
 
 import com.nakaligoba.backend.domain.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,8 @@ import java.util.List;
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 
     List<Photo> findAllByPostId(Long post_id);
+
+    @Query("DELETE FROM Photo p WHERE p.post.id = ?1")
+    @Modifying
+    void deleteAllByPostId(Long postId);
 }
