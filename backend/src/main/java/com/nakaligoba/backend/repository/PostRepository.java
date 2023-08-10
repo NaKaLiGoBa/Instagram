91986950103e3c63c91fc39863db9d0870038946
Daeeun Ko <7273322@naver.com>
2023-08-10 14:24:31 +0900

    feat: 포스트 페이징 리포지토리 메서드 작성

--- backend/src/main/java/com/nakaligoba/backend/repository/PostRepository.java
+++ backend/src/main/java/com/nakaligoba/backend/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.nakaligoba.backend.repository;
 
 import com.nakaligoba.backend.domain.Post;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    Slice<Post> findByIdLessThanOrderByIdDesc(Long startPostId, Pageable pageable);
 }
