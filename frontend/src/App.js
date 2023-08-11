f2b60acd0eede71b4765e6d81b9034f2ffa0b255
mintmin <mintmin0320@gmail.com>
2023-08-11 14:30:25 +0900

    ci/cd: HashRouter 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import MainRoute from "./routes/MainRoute";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <MainRoute />
-    </BrowserRouter>
+    </HashRouter>
   );
-}
+};
 
 export default App;
